4a9869f9f35f58a0b198d3afbfa12b377555be35
Mark Stouffer <=>
2018-03-18 12:18:23 -0700

    rebuilding site Sun, Mar 18, 2018 12:18:23 PM

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@
 printf "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 printf "Build the project."
-# rmdir docs/** -r
-# rm -rf docs/** 
+rmdir docs/** -r
+rm -rf docs/** 
 printf "statistquo.com" > docs/CNAME
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
