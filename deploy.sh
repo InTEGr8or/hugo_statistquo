e04875a2b1f2c427d582d336d7d9d2dcd1336568
Mark Stouffer <=>
2018-03-18 12:03:59 -0700

    rebuilding site Sun, Mar 18, 2018 12:03:59 PM

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@
 # git submodule add -b master git@github.com:InTEGr8or/nofakingway.git public
 # 
 
-printf -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+printf "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-printf -e "Build the project."
+printf "Build the project."
 rmdir docs/** --r
 printf "statistquo.com" > docs/CNAME
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
@@ -30,7 +30,7 @@ git push # origin master -f
 # Come Back up to the Project Root
 # cd ..
 
-printf -e "Task complete. You may close this window..."
+printf "Task complete. You may close this window..."
 
 read
 
